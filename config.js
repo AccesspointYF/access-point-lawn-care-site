961cf241feff03b0760891cb8476048daa687144
AccesspointYF <mopag53@gmail.com>
2026-06-02 10:20:48 -0400

    Refactor APL_CONFIG to CONFIG with Google Script URL

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-window.APL_CONFIG = {
-  // Paste your deployed Google Apps Script /exec URL here if you want live form-to-sheet delivery.
-  LEAD_ENDPOINT: ""
+const CONFIG = {
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwwn9hdiRO8fE0oV5GY6aRiuSaN6jLv9Rmce-dp7BfgQB_l4guvNlzo79Nm_DMN0J_lIQ/exec"
 };
